6991a57dc23acc5fdfc94fba1dfcbb0cca97cb31
Meredith <meredith@getsentry.com>
2019-09-19 09:45:27 -0700

    more mapping

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1446,6 +1446,7 @@ SENTRY_DEFAULT_INTEGRATIONS = (
     "sentry.integrations.jira_server.JiraServerIntegrationProvider",
     "sentry.integrations.vsts.VstsIntegrationProvider",
     "sentry.integrations.vsts_extension.VstsExtensionIntegrationProvider",
+    "sentry.integrations.pagerduty.PagerDutyIntegrationProvider",
 )
 
 
